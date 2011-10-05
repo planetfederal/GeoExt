planetfederal/GeoExt

commit b7457d4cae75730c5d18c66277313793418e848f

fix popups reappearance when panning after being hidden. p=amercade r=bbinet,ahocevar (closes #408)
git-svn-id: http://svn.geoext.org/core/trunk/geoext@2841 5b3c568a-86f9-4a59-a47a-7be284f604b4

lib/GeoExt/widgets/Popup.js
@@ -79,6 +79,11 @@ GeoExt.Popup = Ext.extend(Ext.Window, {
      */
     location: null,
 
+    /** private: property[insideViewport]
+     *  ``Boolean`` Wether the popup is currently inside the map viewport.
+     */
+    insideViewport: null,
+
     /**
      * Some Ext.Window defaults need to be overriden here
      * because some Ext.Window behavior is not currently supported.
@@ -137,6 +142,10 @@ GeoExt.Popup = Ext.extend(Ext.Window, {
             this.location = this.location.getBounds().getCenterLonLat();
         } else if (this.location instanceof OpenLayers.Pixel) {
             this.location = this.map.getLonLatFromViewPortPx(this.location);
+        }
+
+        if (this.location) {
+            this.insideViewport = this.map.getExtent().containsLonLat(this.location);
         }
 
         if(this.anchored) {
@@ -223,9 +232,10 @@ GeoExt.Popup = Ext.extend(Ext.Window, {
      */
     position: function() {
         if(this._mapMove === true) {
-            var visible = this.map.getExtent().containsLonLat(this.location);
-            if(visible !== this.isVisible()) {
-                this.setVisible(visible);
+            this.insideViewport = this.map.getExtent().containsLonLat(this.location);
+            if(this.insideViewport !== this.isVisible()) {
+                this.setVisible(this.insideViewport);
+
             }
         }
 
@@ -310,9 +320,11 @@ GeoExt.Popup = Ext.extend(Ext.Window, {
     /** private: method[onMapMove]
      */
     onMapMove: function() {
-        this._mapMove = true;
-        this.position();
-        delete this._mapMove;
+        if (!(this.hidden && this.insideViewport)){       
+            this._mapMove = true;
+            this.position();
+            delete this._mapMove;
+        }
     },
     
     /** private: method[addAnchorEvents]

tests/lib/GeoExt/widgets/Popup.html
@@ -50,14 +50,15 @@
             context.mapPanel.destroy();
         }
 
-        function popup(feature, map) {
+        function popup(feature, map, hide) {
             return new GeoExt.Popup({
                 title: 'My Popup',
                 location: feature,
                 map: map,
                 width: 200,
                 html: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
-                collapsible: true
+                collapsible: true,
+                closeAction: (hide) ? "hide" : "close"     // Default action is "close"
             });
         }
 
@@ -121,7 +122,6 @@
 
             tearDown(context);
         }
-
 
         function test_unanchorPopup(t) {
             t.plan(4);
@@ -232,6 +232,46 @@
             
             t.ok(!called, "pop is not moved after it is destroyed");
             
+            tearDown(context);
+        }
+
+        function test_hidePopup(t){
+            t.plan(5);
+
+            var context = setupContext();
+            
+            // Reusable popup (closeAction = hide)
+            var pop = popup(context.feature,null,true);
+            
+            pop.show();
+
+            // Center map to popup's location
+            context.map.setCenter(new OpenLayers.LonLat(5, 45));           
+            t.ok(pop.insideViewport && !pop.hidden, "Reusable popup is visible when open and inside the viewport");
+
+            // Pan map outside popup's location
+            context.map.panTo(new OpenLayers.LonLat(170, 80));
+            t.ok(!pop.insideViewport && pop.hidden, "Reusable popup is hidden when open and outside the viewport");
+
+            // Recenter map to popup's location, hide popup and pan map
+            context.map.panTo(new OpenLayers.LonLat(5, 45));
+            pop.hide();
+            context.map.panTo(new OpenLayers.LonLat(6, 45));
+            t.ok(pop.insideViewport && pop.hidden, "Reusable popup is hidden when closed and inside the viewport");          
+
+
+            // Regular popup (destroyed when closed)
+            var pop2 = popup(context.feature);
+            pop2.show();
+
+            // Center map to popup's location
+            context.map.setCenter(new OpenLayers.LonLat(5, 45));           
+            t.ok(pop2.insideViewport && !pop2.hidden, "Regular popup is visible when open and inside the viewport");
+
+            // Pan map outside popup's location
+            context.map.panTo(new OpenLayers.LonLat(170, 80));
+            t.ok(!pop2.insideViewport && pop2.hidden, "Regular popup is hidden when open and outside the viewport");
+
             tearDown(context);
         }
 

commit 31088bcc4b672c500cfad1832a8ec3ec1c176edb

fix regression introduced in r2841: an exception occurred when the popup was created while the mappanel was not rendered yet. r=bartvde (closes #408)
git-svn-id: http://svn.geoext.org/core/trunk/geoext@2850 5b3c568a-86f9-4a59-a47a-7be284f604b4

lib/GeoExt/widgets/Popup.js
@@ -144,8 +144,9 @@ GeoExt.Popup = Ext.extend(Ext.Window, {
             this.location = this.map.getLonLatFromViewPortPx(this.location);
         }
 
-        if (this.location) {
-            this.insideViewport = this.map.getExtent().containsLonLat(this.location);
+        var mapExtent =  this.map.getExtent();
+        if (mapExtent && this.location) {
+            this.insideViewport = mapExtent.containsLonLat(this.location);
         }
 
         if(this.anchored) {

tests/lib/GeoExt/widgets/Popup.html
@@ -9,13 +9,13 @@
 
     <script type="text/javascript">
 
-        function setupContext() {        
+        function setupContext(options) {        
 
             var map = new OpenLayers.Map({panMethod: null}); // avoid tween panning for tests
             var layer = new OpenLayers.Layer("test", {isBaseLayer: true});
             map.addLayer(layer);
 
-            var mapPanel = new GeoExt.MapPanel({
+            var mapPanel = new GeoExt.MapPanel(Ext.apply({
                 // panel options
                 id: "map-panel",
                 title: "GeoExt MapPanel",
@@ -26,7 +26,7 @@
                 map: map,
                 center: new OpenLayers.LonLat(5, 45),
                 zoom: 4
-            });
+            }, options));
 
             var feature = new OpenLayers.Feature.Vector(
                 new OpenLayers.Geometry.Point(5,45),
@@ -50,16 +50,38 @@
             context.mapPanel.destroy();
         }
 
-        function popup(feature, map, hide) {
-            return new GeoExt.Popup({
+        function popup(feature, map, options) {
+            return new GeoExt.Popup(Ext.apply({
                 title: 'My Popup',
                 location: feature,
                 map: map,
                 width: 200,
                 html: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
                 collapsible: true,
-                closeAction: (hide) ? "hide" : "close"     // Default action is "close"
+                closeAction: "close"     // Default action is "close"
+            }, options));
+        }
+
+        function test_beforemaprendered(t) {
+            t.plan(1);
+
+            var pop;
+            var context = setupContext({
+                listeners: {
+                    beforerender: function() {
+                        pop = popup(new OpenLayers.LonLat(5, 45), this.map, {
+                            manager: Ext.WindowMgr
+                        });
+                        return true;
+                    }
+                }
             });
+
+            pop.show();
+
+            t.ok(Ext.getBody().child("div." + pop.popupCls),"viewport contains popup");
+
+            tearDown(context);
         }
 
         function test_addtomappanel(t) {
@@ -241,7 +263,9 @@
             var context = setupContext();
             
             // Reusable popup (closeAction = hide)
-            var pop = popup(context.feature,null,true);
+            var pop = popup(context.feature, null, {
+                closeAction: 'hide'
+            });
             
             pop.show();
 